e1714d0e392e98515254674278a88d499df290ab
Soumyajit Brahma <soumyajitbrah@gmail.com>
2023-02-15 23:11:58 +0530

    added T & C also added all blogs page

--- app.js
+++ app.js
@@ -5,13 +5,19 @@ const path = require('path')
 const bodyparser = require('body-parser')
 // EXPRESS SPECIFIC STUFF
 app.use('/views', express.static('views')) // For serving static files
-app.use(express.urlencoded())
+app.use(express.urlencoded({extended:  true}))
 // EJS SPECIFIC STUFF
 app.set('view engine', 'ejs') // Set the template engine as pug
 app.set('views', path.join(__dirname, 'views')) // Set the views directory
 
 app.get('/', (req, res) => {
   res.render('index.ejs')
+})
+app.get('/blogs', (req, res) => {
+  res.render('allblogspage.ejs')
+})
+app.get('/terms', (req, res) => {
+  res.render('terms.ejs')
 })
 
 app.listen(port, () => {
